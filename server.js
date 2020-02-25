2e82b12747ccc43e3c146bd5972c9cbbccce5061
michael-basweti <baswetima@gmail.com>
2020-02-25 20:39:24 +0300

    CONNECT TO DB

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express();
+
+// connectdb
+
+connectDB()
 
 app.get('/',(req, res) => res.json({msg:"welcom to node"}))
 

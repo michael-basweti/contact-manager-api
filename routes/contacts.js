6b7ddb157e9ec2128e0ef0018058ab63032059ea
michael-basweti <baswetima@gmail.com>
2020-02-26 14:15:17 +0300

    add contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,7 +5,8 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User')
 const Contact = require('../models/Contact')
-
+// 
+// get contacts
 router.get('/', auth, async (req, res) => {
     try {
         const contacts = await Contact.find({user:req.user.id}).sort({date: -1})
@@ -16,14 +17,35 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
-    res.send('Add contacts')
+// add contact
+router.post('/',[auth,[
+    check('name', 'Name is required').not().isEmpty()
+]], async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+    const {name, email, phone, type } = req.body;
+
+    try {
+        const newContact = new Contact({
+            name, email, phone, type, user:req.user.id
+        })
+        const contact = await newContact.save()
+        res.json(contact)
+    } catch (err) {
+        console.error(erro.message);
+        res.status(500).send('server error')
+    }
 });
 
+// edit contact
 router.put('/:id', (req, res) => {
     res.send('update contacts')
 });
 
+// delete contact
 router.delete('/:id', (req, res) => {
     res.send('Delete contact')
 });
